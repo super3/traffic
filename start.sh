cd2d46be5c06e79a2b8b049177ccc01d1253951a
Shawn Wilkinson <me@super3.org>
2020-04-14 21:25:13 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,11 +7,11 @@ funcTerminate() {
   exit 2
 }
 
-mkdir -p /traffic
+mkdir -p /traffic/capture
 captureCam () {
-        mkdir -p ~/traffic/$1
+        mkdir -p /traffic/capture/$1
         t=`date +%s`
-        ffmpeg -t $3 -r 15 -f mjpeg -i $2 /traffic/$1/$1-$t.avi -r 15
+        ffmpeg -t $3 -r 15 -f mjpeg -i $2 /traffic/capture/$1/$1-$t.avi -r 15
 }
 
 tm=15
